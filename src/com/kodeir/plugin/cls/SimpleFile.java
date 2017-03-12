3b2de92f22a346ec4097d0fb748c77eb20a71ac5
Kodeir <kodeir@gmail.com>
2017-03-12 13:34:05 +0300

    SimpleFile defined

---
+++ src/com/kodeir/plugin/cls/SimpleFile.java
@@ -0,0 +1,33 @@
+package com.kodeir.plugin.cls;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * Created by Sergei "Kodeir" Riabinin on 12/03/17.
+ */
+public class SimpleFile extends PsiFileBase {
+    public SimpleFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, SimpleLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return SimpleFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "Simple File";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+}
